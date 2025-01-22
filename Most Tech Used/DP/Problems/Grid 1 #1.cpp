eead3b0471e6e67d8f406d0569dc9a08566c4192
HossamSaberr <140740752+HossamSaberr@users.noreply.github.com>
2025-01-22 22:52:41 +0200

    Add files via upload

---
+++ Most Tech Used/DP/Problems/Grid 1 #1.cpp
@@ -0,0 +1,68 @@
+// atcoder : https://atcoder.jp/contests/dp/tasks/dp_h
+/*
+بسم الله الرحمن الرحيم
+
+الْحَمْدُ لِلّهِ الَّذِي هَدَانَا لِهَـذَا وَمَا كُنَّا لِنَهْتَدِيَ لَوْلا أَنْ هَدَانَا اللّهُ
+
+اللهم حرر فلسطين من كيد الصهاينة واجعل كيدهم في نحورهم
+اللهم انصر الأقصى والقدس وأهالي غزة وفلسطين اللهم اطمس على أعداء الدين والمدنسين
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const long double pii = 3.1415926536;
+#define int long long
+#define endl '\n'
+#define Shity ios_base::sync_with_stdio(false);
+#define Code cin.tie(nullptr);cout.tie(nullptr);
+#define By int t = 1;//cin >> t;
+#define Hossam while(t--) { solve(); }
+#define all(x) begin(x), end(x)
+#define NO cout << "NO" << endl
+#define YES cout << "YES" << endl
+#define init memset(dp , -1 , sizeof dp) // Note : use it with 0 , -1 only (because it fills each "byte" `not the whole int` with 0x0 ansing a 0 or 0xFF ansing -1) AND it make it in O(N) if you use vector use std::fill instead
+
+void Free_Palestine() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int h , w;
+char grid[1005][1005];
+int dp[1005][1005];
+const int MOD = 1e9 + 7;
+
+int idk(int i , int j) {
+    if (i >= h || j >= w || grid[i][j] == '#')
+        return 0;
+    if (i == h - 1 && j == w - 1)
+        return 1;
+    int &ret = dp[i][j];
+    if(~ret)
+        return ret;
+    int ch1 = idk(i + 1 , j);
+    int ch2 = idk(i , j + 1);
+    return ret = (ch1 + ch2) % MOD;
+}
+
+void solve() {
+    cin >> h >> w;
+    for (int i = 0; i < h; ++i) {
+        for (int j = 0; j < w; ++j) {
+            cin >> grid[i][j];
+        }
+    }
+    init;
+    cout << idk(0 , 0) << endl;
+}
+
+int32_t main() {
+    Free_Palestine();
+    Shity Code By Hossam
+    return 0;
+}
+// الحمدلله رب العالمين
