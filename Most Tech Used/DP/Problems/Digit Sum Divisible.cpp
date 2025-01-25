47331a5a3c3e6e5d00cd71fcb04e3e46de5c5200
HossamSaberr <140740752+HossamSaberr@users.noreply.github.com>
2025-01-25 04:30:22 +0200

    Add files via upload

---
+++ Most Tech Used/DP/Problems/Digit Sum Divisible.cpp
@@ -0,0 +1,68 @@
+// atcoder : https://atcoder.jp/contests/abc336/tasks/abc336_e
+/*
+بسم الله الرحمن الرحيم
+
+الْحَمْدُ لِلّهِ الَّذِي هَدَانَا لِهَـذَا وَمَا كُنَّا لِنَهْتَدِيَ لَوْلا أَنْ هَدَانَا اللّهُ
+
+اللهم حرر فلسطين من كيد الصهاينة واجعل كيدهم في نحورهم
+اللهم انصر الأقصى والقدس وأهالي غزة وفلسطين اللهم اطمس على أعداء الدين والمدنسين
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const long double pii = 3.1415926536;
+#define int long long
+#define endl '\n'
+#define Shity ios_base::sync_with_stdio(false);
+#define Code cin.tie(nullptr);cout.tie(nullptr);
+#define By int t = 1;//cin >> t;
+#define Hossam while(t--) { solve(); }
+#define all(x) rbegin(x), rend(x)
+#define NO cout << "NO" << endl
+#define YES cout << "YES" << endl
+#define init memset(dp , -1 , sizeof dp) // Note : Never use it with N = 2 * 1e5 !!! && use it with 0 , -1 only (because it fills each "byte" `not the whole int` with 0x0 ansing a 0 or 0xFF ansing -1) AND it make it in O(N) if you use vector use std::fill instead
+
+void Free_Palestine() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int n;
+string s;
+int dp[20][2][200][200];
+
+int idk(int i, bool mx, int sum, int num , int mod) {
+    if (i == s.size())
+        return (sum == mod && num == 0);
+    int &ret = dp[i][mx][sum][num];
+    if (~ret)
+        return ret;
+    ret = 0;
+    int end = mx ? s[i] - '0' : 9;
+    for (int j = 0; j <= end; ++j) {
+        ret += idk(i + 1, mx && (j == end), sum + j, (num * 10 + j) % mod , mod);
+    }
+    return ret;
+}
+
+void solve() {
+    cin >> n;
+    s = to_string(n);
+    int ans = 0;
+    for (int i = 1; i <= 126; ++i) {
+        init;
+        ans += idk(0 , 1 , 0 , 0 , i);
+    }
+    cout << ans << endl;
+}
+
+int32_t main() {
+    Free_Palestine();
+    Shity Code By Hossam
+    return 0;
+}
+// الحمدلله رب العالمين
