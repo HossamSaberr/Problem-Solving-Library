072878dcfdd01ac8df5efd37113b096346157b61
HossamSaberr <140740752+HossamSaberr@users.noreply.github.com>
2025-02-01 06:16:29 +0200

    Add files via upload

---
+++ Most Tech Used/DP/Problems/Boredom.cpp
@@ -0,0 +1,59 @@
+// Codeforces : https://codeforces.com/contest/455/problem/A
+/*
+بسم الله الرحمن الرحيم
+
+الْحَمْدُ لِلّهِ الَّذِي هَدَانَا لِهَـذَا وَمَا كُنَّا لِنَهْتَدِيَ لَوْلا أَنْ هَدَانَا اللّهُ
+
+اللهم حرر فلسطين من كيد الصهاينة واجعل كيدهم في نحورهم
+اللهم انصر الأقصى والقدس وأهالي غزة وفلسطين اللهم اطمس على أعداء الدين والمدنسين
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const long double pii = 3.1415926536;
+#define int long long
+#define endl '\n'
+#define Shity ios_base::sync_with_stdio(false);
+#define Code cin.tie(nullptr);cout.tie(nullptr);
+#define By int t = 1;//cin >> t;
+#define Hossam while(t--) { solve(); }
+#define all(x) rbegin(x), rend(x)
+#define NO cout << "NO" << endl
+#define YES cout << "YES" << endl
+#define init memset(dp , -1 , sizeof dp) // Note : Never use it with N = 2 * 1e5 !!! && use it with 0 , -1 only (because it fills each "byte" `not the whole int` with 0x0 ansing a 0 or 0xFF ansing -1) AND it make it in O(N) if you use vector use std::fill instead
+
+void Free_Palestine() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+const int N = 1e5 + 5;
+ 
+void solve() {
+    int n;
+    cin >> n;
+    int h[n] , freq[N];
+    memset(freq , 0 , sizeof freq);
+    for (int i = 0; i < n; ++i) {
+        cin >> h[i];
+        freq[h[i]]++;
+    }
+    int dp[N + 1];
+    init;
+    dp[1] = freq[1];
+    for (int i = 2; i <= N - 5; ++i) {
+        dp[i] = max(dp[i - 1] , dp[i - 2] + (i * freq[i]));
+    }
+    cout << dp[N - 5];
+}
+
+int32_t main() {
+    Free_Palestine();
+    Shity Code By Hossam
+    return 0;
+}
+// الحمدلله رب العالمين
