d9acec336d046c2fc5f8b7d6d1fcf92b18d26054
HossamSaberr <140740752+HossamSaberr@users.noreply.github.com>
2025-02-01 11:13:20 +0200

    Add files via upload

---
+++ Most Tech Used/DP/Problems/Chef Monocarp.cpp
@@ -0,0 +1,64 @@
+// Codeforces : https://codeforces.com/contest/1437/problem/C
+/*
+بسم الله الرحمن الرحيم
+
+الْحَمْدُ لِلّهِ الَّذِي هَدَانَا لِهَـذَا وَمَا كُنَّا لِنَهْتَدِيَ لَوْلا أَنْ هَدَانَا اللّهُ
+
+اللهم حرر فلسطين من كيد الصهاينة واجعل كيدهم في نحورهم
+اللهم انصر الأقصى والقدس وأهالي غزة وفلسطين اللهم اطمس على أعداء الدين والمدنسين
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const long double pii = 3.1415926536;
+#define int long long
+#define endl '\n'
+#define Shity ios_base::sync_with_stdio(false);
+#define Code cin.tie(nullptr);cout.tie(nullptr);
+#define By int t = 1;cin >> t;
+#define Hossam while(t--) { solve(); }
+#define all(x) begin(x), end(x)
+#define NO cout << "NO" << endl
+#define YES cout << "YES" << endl
+#define init memset(dp , -1 , sizeof dp) // Note : Never use it with N = 2 * 1e5 !!! && use it with 0 , -1 only (because it fills each "byte" `not the whole int` with 0x0 ansing a 0 or 0xFF ansing -1) AND it make it in O(N) if you use vector use std::fill instead
+
+void Free_Palestine() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int n , h[205] , dp[205][2005];
+
+int idk(int i , int minute) {
+    if(i == n)
+        return 0;
+    int &ret = dp[i][minute];
+    if(~ret)
+        return ret;
+    int ch1 = idk(i + 1 , minute + 1) + abs(h[i] - minute) , ch2 = 1e17;
+    if(minute + 1 < 401)
+        ch2 = idk(i , minute + 1);
+    return ret = min(ch1 , ch2);
+}
+
+void solve() {
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> h[i];
+    }
+    sort(h , h + n);
+    init;
+    cout << idk(0 , 1) << endl;
+}
+
+int32_t main() {
+    Free_Palestine();
+    Shity Code By Hossam
+    return 0;
+}
+
+// الحمدلله رب العالمين
