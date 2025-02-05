88edc503dec387d1b823ef3df32bb9565fa10e2b
HossamSaberr <140740752+HossamSaberr@users.noreply.github.com>
2025-02-05 12:18:07 +0200

    Add files via upload

---
+++ Most Tech Used/Graph/Problems/Bear and Friendship Condition.cpp
@@ -0,0 +1,111 @@
+// Codeforces : https://codeforces.com/contest/771/problem/A
+/*
+بسم الله الرحمن الرحيم
+
+الْحَمْدُ لِلّهِ الَّذِي هَدَانَا لِهَـذَا وَمَا كُنَّا لِنَهْتَدِيَ لَوْلا أَنْ هَدَانَا اللّهُ
+
+اللهم حرر فلسطين من كيد الصهاينة واجعل كيدهم في نحورهم
+اللهم انصر الأقصى والقدس وأهالي غزة وفلسطين اللهم اطمس على أعداء الدين والمدنسين
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const long double pii = 3.1415926536;
+#define int long long
+#define endl '\n'
+#define Shity ios_base::sync_with_stdio(false);
+#define Code cin.tie(nullptr);cout.tie(nullptr);
+#define By int t = 1;//cin >> t;
+#define Hossam while(t--) { solve(); }
+#define all(x) begin(x), end(x)
+#define NO cout << "NO" << endl
+#define YES cout << "YES" << endl
+#define init memset(dp , -1 , sizeof dp) // Note : Never use it with N = 2 * 1e5 !!! && use it with 0 , -1 only (because it fills each "byte" `not the whole int` with 0x0 ansing a 0 or 0xFF ansing -1) AND it make it in O(N) if you use vector use std::fill instead
+
+void Free_Palestine() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int n , m;
+const int N = 1e6 + 5;
+vector<int> adj[N];
+vector<int> h(N);
+vector<bool> vis(N);
+vector<bool> vis2(N);
+vector<int> color(N);
+vector<int> deg(N);
+vector<int> connected(N);
+
+void dfs(int node) {
+    vis[node] = true;
+    connected.push_back(node);
+    for(auto to : adj[node]) {
+        if(!vis[to]) {
+            dfs(to);
+        }
+    }
+}
+
+bool Cycle(int node , int parent) {
+    vis[node] = true;
+    for (auto to : adj[node]) {
+        if (!vis[to]) {
+            if (Cycle(to, node))
+                return true;
+        }
+        else if (to != parent)
+            return true;
+    }
+    return false;
+}
+
+bool Bipartite(int node) {
+    bool ans = true;
+    for(auto neig : adj[node]) {
+        if(!color[neig]) {
+            color[neig] = color[node] == 1 ? 2 : 1;
+            ans &= Bipartite(neig);
+        }
+        else if(color[neig] == color[node])
+            return false;
+    }
+    return ans;
+}
+
+
+void solve() {
+    cin >> n >> m;
+    set<int> st;
+    for(int i = 0; i < m; i++) {
+        int u , v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+        st.insert(u);
+        st.insert(v);
+    }
+    for(auto node : st) {
+        if(!vis[node]) {
+            connected.clear();
+            dfs(node);
+        }
+        for(auto it : connected) {
+            if(adj[it].size() != connected.size() - 1)
+                return void(NO);
+        }
+    }
+    YES;
+}
+
+int32_t main() {
+    Free_Palestine();
+    Shity Code By Hossam
+    return 0;
+}
+
+// الحمدلله رب العالمين
